6c6312766294ffa355667513d8a3e0161617db1b
Mudit Porwal <58726437+mdtgupta939@users.noreply.github.com>
2021-02-16 14:50:37 +0530

    update

---
+++ mongo-tools.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo --Adding repo for mongo-tools
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
+apt-get update
+
+echo --installing mongo-tools
+apt-get install -y mongodb-org
